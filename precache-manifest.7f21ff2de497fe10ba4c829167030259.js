cb983434265a07d963c1e5b22931c730bc880d25
Rachel Mahan <rachelm2490@gmail.com>
2018-12-10 13:30:42 -0500

    Updates

---
+++ precache-manifest.7f21ff2de497fe10ba4c829167030259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74eb8b249915e38cfe6a",
+    "url": "/maha2490/like-so-festive-2018/static/js/runtime~main.74eb8b24.js"
+  },
+  {
+    "revision": "1c0cf04a55158b5f60d7",
+    "url": "/maha2490/like-so-festive-2018/static/js/main.1c0cf04a.chunk.js"
+  },
+  {
+    "revision": "0b4a6a52a16f569a5947",
+    "url": "/maha2490/like-so-festive-2018/static/js/1.0b4a6a52.chunk.js"
+  },
+  {
+    "revision": "1c0cf04a55158b5f60d7",
+    "url": "/maha2490/like-so-festive-2018/static/css/main.6cc6a279.chunk.css"
+  },
+  {
+    "revision": "e670e9d4591ce5cf7dd56d69dc88a17a",
+    "url": "/maha2490/like-so-festive-2018/index.html"
+  }
+];
